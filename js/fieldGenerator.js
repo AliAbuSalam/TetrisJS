5d45cd5913b20075befe3e480c57a36ba2665722
Ali Abu Salam <aliadadehmav@gmail.com>
2025-03-08 16:00:17 +0700

    change initial fieldGenerator

--- js/fieldGenerator.js
+++ js/fieldGenerator.js
@@ -1,15 +1,21 @@
-console.log('generating field...');
-const generateField = (numberOfBlocks) => {
-  const fieldContainer = document.getElementById('field');
+const generateField = (rows, hiddenRows, columns) => {
+  const fieldContainer = document.getElementsByClassName('field')[0];
+  console.log(`rows: ${rows}`);
+  console.log(`hiddenRows: ${hiddenRows}`);
+  console.log(`columns: ${columns}`)
 
-  for(i = 0;i < numberOfBlocks; i++ ){
-    const block = document.createElement('div')
-    block.className = 'field-block'
-    block.id = `block${i+1}`
-    fieldContainer.appendChild(block);
+  for(let i = rows - 1; i >= 0; i--){
+    const row = document.createElement('div');
+    for(let j = 0; j < columns; j++){
+      const block = document.createElement('div');
+      const blockNumber = document.createTextNode(`${(i * 10) + (j + 1)}`);
+      block.appendChild(blockNumber);
+      block.className = 'field-block';
+      block.id = `block${j+1}`;
+      row.appendChild(block);
+    }
+    fieldContainer.appendChild(row);
   }
-  
 }
 
-
-generateField(220)
+export default generateField;
