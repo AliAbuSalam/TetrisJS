8bdd483205125c60ebffb580e19e2c52b0b65f16
Ali Abu Salam <aliadadehmav@gmail.com>
2025-03-16 16:48:24 +0700

    create initial Row class

--- js/class/Row.js
+++ js/class/Row.js
@@ -1,5 +1,16 @@
 class Row {
   constructor(rowNumber){
     this.id = `row-${rowNumber}`;
+
+    const getBlockElements = (rowNumber) => {
+      const blockArray = [];
+      for(let i = 1; i <= 10; i++){
+        const block = Document.GetElementById(rowNumber * 10 + i);
+        blockArray.push(block);
+      }
+      return blockArray;
+    }
+    
+    this.blocks = getBlockElements(rowNumber);
   }
 }
