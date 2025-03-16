e975e17a102d10b47d89320bcc2cf61a7d7bc189
Ali Abu Salam <aliadadehmav@gmail.com>
2025-03-16 17:23:30 +0700

    move the implementation of creating rows to Row class instead of fieldGenerator

--- js/class/Row.js
+++ js/class/Row.js
@@ -1,17 +1,23 @@
 class Row {
   constructor(rowNumber){
     this.id = `row-${rowNumber}`;
+    this.rowElement = Document.createElement('div');
 
-    const getBlockElements = (rowNumber) => {
+    const createBlockElements = (rowNumber) => {
       const blockArray = new Map();
       for(let i = 1; i <= 10; i++){
-        const block = Document.GetElementById(rowNumber * 10 + i);
+        const block = Document.createElement('div');
+        block.id = `block-${(rowNumber * 10) + i}`;
+        const blockNumber= document.createTextNode(`${(rowNumber * 10) + i}`);
+        block.appendChild(blockNumber);
+        block.className = 'field-block';
         blockArray.set(i, block);
+        this.rowElement.appendChild(block);
       }
       return blockArray;
     }
     
-    this.blocks = getBlockElements(rowNumber);
+    this.blocks = createBlockElements(rowNumber);
   }
 }
 
