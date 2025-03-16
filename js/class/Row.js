4342cf60cd32309bdc7b016294e755fe111526d2
Ali Abu Salam <aliadadehmav@gmail.com>
2025-03-16 18:58:47 +0700

    implement number of columns when creating row object

--- js/class/Row.js
+++ js/class/Row.js
@@ -3,12 +3,12 @@ class Row {
     this.id = `row-${rowNumber}`;
     this.rowElement = Document.createElement('div');
 
-    const createBlockElements = (rowNumber) => {
+    const createBlockElements = (rowNumber, columnNumber) => {
       const blockArray = new Map();
-      for(let i = 1; i <= 10; i++){
+      for(let i = 1; i <= columnNumber; i++){
         const block = Document.createElement('div');
-        block.id = `block-${(rowNumber * 10) + i}`;
-        const blockNumber= document.createTextNode(`${(rowNumber * 10) + i}`);
+        block.id = `block-${(rowNumber * columnNumber) + i}`;
+        const blockNumber= document.createTextNode(`${(rowNumber * columnNumber) + i}`);
         block.appendChild(blockNumber);
         block.className = 'field-block';
         blockArray.set(i, block);
