43799dd3b4cf47f0cc889dd7611142a7b3853467
Ali Abu Salam <aliadadehmav@gmail.com>
2025-05-03 07:31:12 +0700

    Add hidden rows to field class (kinda)

--- js/class/Field.js
+++ js/class/Field.js
@@ -1,8 +1,13 @@
 import VisibleRow from './VisibleRow.js';
+import HiddenRow from './HiddenRow.js';
 
 class Field {
   constructor(rows, columns, hiddenRows){
     this.field = document.getElementsByClassName('field')[0];
+    for(let i = hiddenRows + rows; i > rows; i--){
+      const rowObj = new HiddenRow(i, columns);
+      //not sure how i'm gonna deal with hidden row yet
+    }
     for(let i = rows - 1; i >= 0; i--){
       const rowObj = new VisibleRow(i, columns);
       this.field.appendChild(rowObj.rowElement);
