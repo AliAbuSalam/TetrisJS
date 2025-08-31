9f14d1742eeda5a639ea627e7709f83266f76084
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-01 05:53:30 +0700

    use columns, hidden rows, and visible rows as constant for generating field

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 import generateField from './fieldGenerator.js'
-generateField(20,20,10);
+import PiecesGenerator from './class/PiecesGenerator.js'
+import { NUMBER_OF_COLUMNS, NUMBER_OF_HIDDEN_ROWS, NUMBER_OF_VISIBLE_ROWS } from './CONSTANTS.js';
+generateField(NUMBER_OF_VISIBLE_ROWS, NUMBER_OF_HIDDEN_ROWS, NUMBER_OF_COLUMNS);
+
+const piecesGenerator = new PiecesGenerator();
+piecesGenerator.generateNewPool();
