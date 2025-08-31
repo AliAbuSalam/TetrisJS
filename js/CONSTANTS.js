f5c66f9e8ff0b016444a3426328138aeeceb176e
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-01 05:50:20 +0700

    add visible rows, hidden rows, and columns as constant

--- js/CONSTANTS.js
+++ js/CONSTANTS.js
@@ -1,1 +1,4 @@
-export const NUMBER_OF_PIECES = 5;
+export const NUMBER_OF_PIECES = 7;
+export const NUMBER_OF_VISIBLE_ROWS = 20;
+export const NUMBER_OF_HIDDEN_ROWS = 5;
+export const NUMBER_OF_COLUMNS = 10;
