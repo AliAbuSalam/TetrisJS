f468d3f0151d06dac002262353fd117dc4b6c686
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-03 07:01:18 +0700

    change the default shape of I Piece class

--- js/class/pieces/PieceI.js
+++ js/class/pieces/PieceI.js
@@ -1,14 +1,15 @@
 import Piece from './Piece.js';
+import { NUMBER_OF_COLUMNS } from '../../CONSTANTS.js';
 
 class PieceI extends Piece {
   constructor(coordinates){
     super();
     this.name = 'I Piece';
-    this.location = [coordinates, coordinates+1, coordinates+2, coordinates+3];
+    this.location = [coordinates, coordinates-NUMBER_OF_COLUMNS, coordinates-2*NUMBER_OF_COLUMNS, coordinates-3*NUMBER_OF_COLUMNS];
   }
 
   rotate(){
-
+    
   }
 
   move(){
