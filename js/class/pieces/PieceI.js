c75d8f9fb0bdf47206b7dd8f1bce870c136f1a37
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-03 07:14:19 +0700

    implement rotate method of I Piece class

--- js/class/pieces/PieceI.js
+++ js/class/pieces/PieceI.js
@@ -5,11 +5,19 @@ class PieceI extends Piece {
   constructor(coordinates){
     super();
     this.name = 'I Piece';
-    this.location = [coordinates, coordinates-NUMBER_OF_COLUMNS, coordinates-2*NUMBER_OF_COLUMNS, coordinates-3*NUMBER_OF_COLUMNS];
+    this.location = [coordinates+NUMBER_OF_COLUMNS, coordinates, coordinates-1*NUMBER_OF_COLUMNS, coordinates-2*NUMBER_OF_COLUMNS];
   }
 
   rotate(){
-    
+    const pp = this.pivotPoint //shortening pivotPoint;
+    if(state === 'default'){
+      this.location = [pp - 1, pp, pp + 1, pp + 2];
+      this.state = 'rotated';
+    } else {
+      this.location = [pp + NUMBER_OF_COLUMNS, pp, pp-1*NUMBER_OF_COLUMNS, pp-2*NUMBER_OF_COLUMNS]
+      this.state = 'default';
+    }
+    return [...this.location];
   }
 
   move(){
