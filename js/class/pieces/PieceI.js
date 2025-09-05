607733a50cb314e989f62f592f8aec3a85261327
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-05 09:28:02 +0700

    fix bug on I Piece constructor

--- js/class/pieces/PieceI.js
+++ js/class/pieces/PieceI.js
@@ -3,7 +3,7 @@ import { NUMBER_OF_COLUMNS } from '../../CONSTANTS.js';
 
 class PieceI extends Piece {
   constructor(coordinates){
-    super();
+    super(coordinates);
     this.name = 'I Piece';
     this.location = [coordinates+NUMBER_OF_COLUMNS, coordinates, coordinates-1*NUMBER_OF_COLUMNS, coordinates-2*NUMBER_OF_COLUMNS];
   }
