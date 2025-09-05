d67e317d3e000c09b17a553f95046496a5fd6c15
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-05 08:30:49 +0700

    add basic rotate method for Piece class

--- js/class/pieces/Piece.js
+++ js/class/pieces/Piece.js
@@ -20,8 +20,17 @@ class Piece {
     return [...this.location];
   }
 
-  rotate(){
-    throw new Error('Method rotate() must be implemented')
+  rotate(objectShapePerState){
+    //objectShapePerState structure: { default: [objectShape], rotated: [objectShape] }
+    if(this.constructor === 'Piece') throw new Error('Rotate method can\'t be called on Abstract Class');
+    const pp = this.pivotPoint
+    if(state === 'default'){
+      this.location = objectShapePerState.rotated;
+      this.state === 'rotated'
+    } else {
+      this.location = objectShapePerState.default;
+      this.state === 'default';
+    }
   }
 
   moveDown(){
