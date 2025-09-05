c7553c927e8e99df7419c608cf0e8b1034dc6e4a
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-05 08:33:14 +0700

    ;

--- js/class/pieces/Piece.js
+++ js/class/pieces/Piece.js
@@ -23,10 +23,10 @@ class Piece {
   rotate(objectShapePerState){
     //objectShapePerState structure: { default: [objectShape], rotated: [objectShape] }
     if(this.constructor === 'Piece') throw new Error('Rotate method can\'t be called on Abstract Class');
-    const pp = this.pivotPoint
+    const pp = this.pivotPoint;
     if(state === 'default'){
       this.location = objectShapePerState.rotated;
-      this.state === 'rotated'
+      this.state === 'rotated';
     } else {
       this.location = objectShapePerState.default;
       this.state === 'default';
