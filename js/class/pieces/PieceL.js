a454a23c37a70cf9b346cd6effe89f9bd1b8dca5
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-06 08:11:13 +0700

    add rotate method to L Piece

--- js/class/pieces/PieceL.js
+++ js/class/pieces/PieceL.js
@@ -1,4 +1,5 @@
 import Piece from './Piece.js';
+import { NUMBER_OF_COLUMNS } from '../../CONSTANTS.js';
 
 class PieceL extends Piece {
   constructor(coordinates){
@@ -7,7 +8,15 @@ class PieceL extends Piece {
     this.location = [coordinates, coordinates-10, (coordinates-10)-1, (coordinates-10)-2]
   }
 
-  rotate(){}
+  rotate(){
+    const pp = this.pivotPoint;
+     const objectShapePerState = {
+      default: [pp + NUMBER_OF_COLUMNS, pp, pp - 1, pp - 2],
+      rotated: [pp + 2*NUMBER_OF_COLUMNS, pp + NUMBER_OF_COLUMNS, pp, pp + 1]
+    }
+    super.rotate(objectShapePerState);
+    return [...this.location];
+  }
   move(){}
 }
 
