e1147d9d0751e08faf7312787e306c200b361481
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-07 09:04:12 +0700

    change I Piece location data structure to follow the new coordinates data structure

--- js/class/pieces/PieceI.js
+++ js/class/pieces/PieceI.js
@@ -3,9 +3,19 @@ import { NUMBER_OF_COLUMNS } from '../../CONSTANTS.js';
 
 class PieceI extends Piece {
   constructor(coordinates){
-    super(coordinates);
     this.name = 'I Piece';
-    this.location = [coordinates+NUMBER_OF_COLUMNS, coordinates, coordinates-1*NUMBER_OF_COLUMNS, coordinates-2*NUMBER_OF_COLUMNS];
+    this.indexOfPivotPoint = 1;
+    this.location = [{
+      ...coordinates, 
+      y: coordinates.y - 2
+      },
+       coordinates,
+      { ...coordinates, 
+        y: coordinates.y - 1
+      }, 
+      {...coordinates, 
+        y: coordinates.y - 3
+      }]
   }
 
   rotate(){    
