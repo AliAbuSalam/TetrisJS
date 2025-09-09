37b5bc4f11e859bbb5e8757874a6c9b08f00de05
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-09 10:31:22 +0700

    add base color for Z Piece class

--- js/CONSTANTS.js
+++ js/CONSTANTS.js
@@ -9,3 +9,4 @@ export const L_PIECE_COLOR = '#1C31BD';
 export const O_PIECE_COLOR = '#C72BCC';
 export const S_PIECE_COLOR = '#2BCCBF';
 export const T_PIECE_COLOR = '#F2E316';
+export const Z_PIECE_COLOR = '#ED9018';
--- js/class/pieces/PieceZ.js
+++ js/class/pieces/PieceZ.js
@@ -1,10 +1,12 @@
 import Piece from './Piece.js';
+import { Z_PIECE_COLOR } from '../../CONSTANTS.js';
 
 class PieceZ extends Piece {
   constructor(coordinates){
     super();
     this.name = 'Z Piece';
     this.indexOfPivotPoint = 0;
+    this.color = Z_PIECE_COLOR;
     this.location = [{
       ...coordinates,
       y: coordinates.y - 1
