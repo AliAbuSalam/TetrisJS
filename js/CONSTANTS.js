86d1b379424def244ce693115b067c1fb91d5145
Ali Abu Salam <aliadadehmav@gmail.com>
2025-09-09 10:20:35 +0700

    add base color for I Piece class

--- js/CONSTANTS.js
+++ js/CONSTANTS.js
@@ -2,3 +2,5 @@ export const NUMBER_OF_PIECES = 7;
 export const NUMBER_OF_VISIBLE_ROWS = 20;
 export const NUMBER_OF_HIDDEN_ROWS = 5;
 export const NUMBER_OF_COLUMNS = 10;
+
+export const I_PIECE_COLOR = '#DE4B28';
--- js/class/pieces/PieceI.js
+++ js/class/pieces/PieceI.js
@@ -1,11 +1,12 @@
 import Piece from './Piece.js';
-import { NUMBER_OF_COLUMNS } from '../../CONSTANTS.js';
+import { I_PIECE_COLOR } from '../../CONSTANTS.js';
 
 class PieceI extends Piece {
   constructor(coordinates){
     super();
     this.name = 'I Piece';
     this.indexOfPivotPoint = 0;
+    this.color = I_PIECE_COLOR;
     this.location = [{
       ...coordinates, 
       y: coordinates.y - 2
