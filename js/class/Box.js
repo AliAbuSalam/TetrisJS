35cf2bb7cc7f02388104b74d387c0414ae8b87c7
Ali Abu Salam <aliadadehmav@gmail.com>
2026-03-06 22:53:02 +0700

    divide box element into 5 smaller element for styling blocks later

--- js/class/Box.js
+++ js/class/Box.js
@@ -7,11 +7,30 @@ class Box {
     this.currentState = 'empty';
 
     this.element = document.createElement('div');
+    this.leftSide = document.createElement('div');
+    this.rightSide = document.createElement('div');
+    this.topSide = document.createElement('div');
+    this.bottomSide = document.createElement('div');
+    this.centerSide = document.createElement('div');
+
+    const boxPartVertical = [this.leftSide, this.rightSide];
+    const boxPartHorizontal = [this.topSide, this.bottomSide];
+    
+    boxPartHorizontal.forEach(part => part.classList.add('box-child-horizontal'));
+    boxPartVertical.forEach(part => part.classList.add('box-child-vertical'));
+    this.element.classList.add('box-parent');
+    this.leftSide.classList.add('box-left');
+    this.rightSide.classList.add('box-right');
+    this.topSide.classList.add('box-top');
+    this.bottomSide.classList.add('box-bottom');
+    this.centerSide.classList.add('box-center');
+    // LET'S TRY TO MAKE MULTIPLE TRIANGLE STACK ON TOP EACH OTHER INSTEAD OF THIS!!!!!!
+    
+    this.element.append(this.leftSide, this.rightSide, this.topSide, this.centerSide, this.bottomSide);
+
     this.id = `block-${id}`;
     this.element.id = this.id;
-    // const blockNumber= document.createTextNode(id);
-    // this.element.appendChild(blockNumber);
-    this.element.className = 'field-block';
+    this.element.classList.add('field-block');
   }
 
   get location() {
